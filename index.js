57701d217f7559952b5982b6b839bac8dab91196
liz <rhodes.liz@gmail.com>
2022-07-21 15:52:08 -0400

    commit tree 643b63dc02efda6b3fb64bc0a6f98067b574b791

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+ return fetch ('https://anapioficeandfire.com/api/books')
+    .then (resp => resp.json())
+    .then (books => {
+      renderBooks(books)
+    })
   
 }
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(books){
+//books.forEach()
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  //fetchBooks();
+
